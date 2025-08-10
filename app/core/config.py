1ae0b976466360b432bfe8339d2596825ddb96e2
irawnewton <gaurabroy16@gmail.com>
2025-08-10 13:58:57 +0530

    update files

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 ENV_PATH = BASE_DIR / ".env"
 
-print(f"Loading .env from: {ENV_PATH}")  # debug
 
 load_dotenv(dotenv_path=ENV_PATH)
 
@@ -16,4 +15,3 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
-print("Loaded DB Name:", MONGO_DB_NAME)  # debug
